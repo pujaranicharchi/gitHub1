2cb0e6b79496430511f465986c8b4300d8f2f384
puja <charchilaxmi@gmail.com>
2026-03-11 17:49:50 +0530

    second commit

---
+++ src/main/java/com/gitHub2/GitHub2Application.java
@@ -0,0 +1,13 @@
+package com.gitHub2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHub2Application.class, args);
+	}
+
+}
